6ccc4767664a4936d4a37605021daa8f018d2ef8
Mohammed-Azhan <azhanpalli@gmail.com>
2025-05-19 12:39:12 +0530

    updated

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -6,7 +6,7 @@ const User = () => {
     const { key } = useParams();
     useEffect(() => {
        const returnOriginal = async () => {
-           const response = await axios.post("https://shortner-2odfd7hv3-mohammed-azhans-projects.vercel.app" ,{url : key});
+           const response = await axios.post("https://urlshortner-backend-ono5.onrender.com/shortenurl" ,{url : key});
            console.log(response.data)
            if(response.data.status){
             return window.location.href = response.data.url;
